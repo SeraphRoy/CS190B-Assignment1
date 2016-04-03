2f9ac54a00bcccb61218b7ec6a7d4b2302e4c594
Baby Pepper <royxagain@gmail.com>
2016-04-02 17:13:26 -0700

    haha finish

--- Assignment1/src/tasks/TaskEuclideanTsp.java
+++ Assignment1/src/tasks/TaskEuclideanTsp.java
@@ -17,49 +17,68 @@ public class TaskEuclideanTsp implements Task<List<Integer>>{
     }
 
     public List<Integer> Execute(){
-        ArrayList<Integer> cityNum = new ArrayList<>();
+        int[] cityNum = new int[cityCo.length];
         for(int i = 0; i < cityCo.length; i++)
-            cityNum.add(i);
+            cityNum[i] = i;
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> solution = new ArrayList<>();
-        int[] visited = new int[cityCo.length];
 
-        HelpExecute(cityNum, 0, result, visited, solution);
-
+        permute(cityNum, 0, result);
         double min = Double.MAX_VALUE;
         double current = 0;
-        solution.clear();
+
+        ArrayList<Integer> solution = new ArrayList<>();
+
         for(ArrayList<Integer> item : result){
             for(int i = 0; i < item.size() -1 ; i++){
-                current += Math.sqrt(Math.pow(cityCo[i+1][0]-cityCo[i][0], 2) + Math.pow(cityCo[i+1][1] - cityCo[i][1], 2));
+                current += Math.sqrt(Math.pow(cityCo[item.get(i+1)][0]-cityCo[item.get(i)][0], 2) + Math.pow(cityCo[item.get(i+1)][1] - cityCo[item.get(i)][1], 2));
             }
-            current += Math.sqrt(Math.pow(cityCo[0][0]-cityCo[item.size()-1][0], 2) + Math.pow(cityCo[0][1] - cityCo[item.size()-1][1], 2));
+            current += Math.sqrt(
+                                 Math.pow(cityCo[item.get(0)][0]-cityCo[item.get(item.size()-1)][0], 2)
+                                 + Math.pow(cityCo[item.get(0)][1] - cityCo[item.get(item.size()-1)][1], 2)
+                                 );
             if (current < min){
                 min = current;
-                for(int i = 0; i < item.size(); i++){
-                    solution.add(i);
-                }
-            }
-            else{
                 solution.clear();
+                for(int i = 0; i < item.size(); i++){
+                    solution.add(item.get(i));
+                }
             }
             current = 0;
         }
-        return solution;
+        return (List<Integer>) solution;
     }
 
-    private void HelpExecute(ArrayList<Integer> cityNum, int step, ArrayList<ArrayList<Integer>> result, int[] visited, ArrayList<Integer> solution){
-        if(step == cityNum.size()){
-            result.add(solution);
-            return;
+    private void permute(int[] num, int start, ArrayList<ArrayList<Integer>> result) {
+
+        if (start >= num.length) {
+            ArrayList<Integer> item = convertArrayToList(num);
+            result.add(item);
+
         }
 
-        for(int i = 0; i < cityNum.size(); i++){
-            visited[i] = 1;
-            solution.add(cityNum.get(i));
-            HelpExecute(cityNum, step+1, result, visited, solution);
-            solution.remove(solution.size()-1);
-            visited[i] = 0;
+        for (int j = start; j <= num.length - 1; j++) {
+            swap(num, start, j);
+            permute(num, start + 1, result);
+            swap(num, start, j);
+
         }
+
+    }
+
+    private ArrayList<Integer> convertArrayToList(int[] num) {
+        ArrayList<Integer> item = new ArrayList<Integer>();
+        for (int h = 0; h < num.length; h++) {
+            item.add(num[h]);
+
+        }
+        return item;
+
+    }
+
+    private void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+
     }
 }
