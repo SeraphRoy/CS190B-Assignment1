5884b801ed6692dedb70788489177de28cec09ac
Baby Pepper <royxagain@gmail.com>
2016-05-28 21:10:52 -0700

    now can run

--- Assignment5/src/system/ResultHandler.java
+++ Assignment5/src/system/ResultHandler.java
@@ -15,62 +15,67 @@ public class ResultHandler implements Runnable{
 
     public void run(){
         while(true){
-            if(resultQ.size() != 0){
-                BlockingQueue<ResultWrapper> temp = null;
-                synchronized(resultQ){
-                    temp = resultQ;
-                    resultQ = new LinkedBlockingQueue<>();
-                }
+            // if(resultQ.size() != 0){
+            //     BlockingQueue<ResultWrapper> temp = null;
+            //     synchronized(resultQ){
+            //         temp = resultQ;
+            //         resultQ = new LinkedBlockingQueue<>();
+            //     }
+            //     try{
+            //         temp.peek().space.putComputerResults(temp);
+            //     }
+            //     catch(RemoteException | InterruptedException e){
+            //         e.printStackTrace();
+            //     }
+            // }
+
+            ResultWrapper result = null;
+            try{
+                result = resultQ.take();
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            if(result.type == 0){
                 try{
-                    temp.peek().space.putComputerResults(temp);
+                    result.space.sendArgument(result.cont);
                 }
                 catch(RemoteException | InterruptedException e){
+                    System.err.println("error from result type0");
                     e.printStackTrace();
                 }
             }
-            // ResultWrapper result = null;
-            // try{
-            //     result = resultQ.take();
-            // }
-            // catch(InterruptedException e){
-            //     e.printStackTrace();
-            // }
-            // if(result.type == 0){
-            //     try{
-            //         result.space.sendArgument(result.cont);
-            //     }
-            //     catch(RemoteException | InterruptedException e){
-            //         System.err.println("error from result type0");
-            //         e.printStackTrace();
-            //     }
-            // }
-            // else if(result.type == 1){
-            //     try{
-            //         if(!result.needToUpdate)
-            //             result.space.sendArgument(result.cont, result.result);
-            //         else
-            //             result.space.sendArgument(result.cont, result.result, result.task.computer.getShare());
-            //     }
-            //     catch(RemoteException | InterruptedException e){
-            //         System.err.println("Error in sending arguments");
-            //         e.printStackTrace();
-            //     }
-            // }
-            // else{
-            //     try{
-            //         result.space.putSpawnResult(result.spawnResult);
-            //     }
-            //     catch(RemoteException | InterruptedException e){
-            //         System.err.println("Error in putting spawn result");
-            //         e.printStackTrace();
-            //     }
-            // }
-            // try{
-            //     result.space.putDoneTask(result.task);
-            // }
-            // catch(Exception e){
-            //     e.printStackTrace();
-            // }
+            else if(result.type == 1){
+                try{
+                    if(!result.needToUpdate){
+                        Argument argument = new Argument(result.result, result.cont.getSlot());
+                        result.space.sendArgument(result.cont, argument);
+                    }
+                    else{
+                        Argument argument = new Argument(result.result, result.cont.getSlot());
+                        result.space.sendArgument(result.cont, argument, new Share(result.task.generateShareValue(result.result)));
+                    }
+                }
+                catch(RemoteException | InterruptedException e){
+                    System.err.println("Error in sending arguments");
+                    e.printStackTrace();
+                }
+            }
+            else{
+                try{
+                    result.space.putSpawnResult(result.spawnResult);
+                }
+                catch(RemoteException | InterruptedException e){
+                    System.err.println("Error in putting spawn result");
+                    e.printStackTrace();
+                }
+            }
+            try{
+                result.space.putDoneTask(result.task);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
         }
     }
 }
